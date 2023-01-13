acbdf0818c84955e5609fcf8de6f73a7c23f4014
John Lenz <john.lenz27@gmail.com>
2023-01-13 13:45:01 -0800

    it will change the forum script to get specific prompts

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@
 
 function hideResults() {
     document.getElementById("error-message").setAttribute("class", "hidden");
-    document.getElementById("sarah").setAttribute("class", "hidden");
-    document.getElementById("ashley").setAttribute("class", "hidden");
-    document.getElementById("emily").setAttribute("class", "hidden");
+    document.getElementById("java").setAttribute("class", "hidden");
+    document.getElementById("html").setAttribute("class", "hidden");
+    document.getElementById("python").setAttribute("class", "hidden");
     document.getElementById("sorry").setAttribute("class", "hidden");
 }
 
@@ -14,25 +14,31 @@ window.onload = function () {
         hideResults();
         const age = parseInt(document.querySelector("input#age").value);
         const height = parseInt(document.querySelector("input#height").value);
+        const radioSelection = document.querySelector("input[name='fav_color']:checked").value
 
         
-        if (age && height) {
+        if(radioSelection === "yellow"){
+            document.getElementById("python").removeAttribute("class");        }
+        
+        
+        
+        // if (age && height) {
 
-            if (age >= 50 && height >= 150) {
-                document.getElementById("sarah").removeAttribute("class");
+        //     if (age >= 50 && height >= 150) {
+        //         document.getElementById("java").removeAttribute("class");
 
-            } else if (age >= 35 && height >= 95) {
-                document.getElementById("ashley").removeAttribute("class");
+        //     } else if (age >= 35 && height >= 95) {
+        //         document.getElementById("html").removeAttribute("class");
 
-            } else if (age >= 20 && height >= 50) {
-                document.getElementById("emily").removeAttribute("class");
+        //     } else if (age >= 20 && height >= 50) {
+        //         document.getElementById("python").removeAttribute("class");
 
-            } else {
-                document.getElementById("sorry").removeAttribute("class");
-            }
-        } else {
-            document.getElementById("error-message").removeAttribute("class");
-        }
+        //     } else {
+        //         document.getElementById("sorry").removeAttribute("class");
+        //     }
+        // } else {
+        //     document.getElementById("error-message").removeAttribute("class");
+        // }
 
 
     };
