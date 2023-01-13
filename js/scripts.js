11bdffd1938215527b3bec5ab4d01d6a4e7caa64
John Lenz <john.lenz27@gmail.com>
2023-01-13 15:53:16 -0800

    it will fix bugs in if statements

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,11 @@ window.onload = function () {
     document.querySelector("form").onsubmit = function (event) {
         event.preventDefault();
         hideResults();
-        const age = parseInt(document.querySelector("input#age").value);
+        // const age = parseInt(document.querySelector("input#age").value);
         const radioSelection = document.querySelector("input[name='favColor']:checked").value
 
         
-        if (age >= 70){
-            document.getElementById("sorry").removeAttribute("class");         
-        
-        } else if(radioSelection === "red"){
+        if(radioSelection === "red"){
             document.getElementById("java").removeAttribute("class");        
 
         }else if (radioSelection === "green"){
